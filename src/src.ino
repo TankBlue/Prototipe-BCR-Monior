6c3383deee1db66193925b6c98959e7ab9eae102
Hasan Basri <hasanbasri1493@gmail.com>
2018-09-24 11:25:13 +0700

    edit uno

--- src/src.ino
+++ src/src.ino
@@ -10,27 +10,37 @@
 #include <BlynkSimpleShieldEsp8266.h>
 #include <SoftwareSerial.h>
 
+#include <SD.h>
+#include <SPI.h>
+#include <DS3231.h>
+
 //Konfigurasi variable - variable
 SimpleTimer timer;
-SoftwareSerial EspSerial(3, 2); //Konfigurasi PIN SoftSerial
-ESP8266 wifi(&EspSerial);
+//SoftwareSerial EspSerial(31, 30); //Konfigurasi PIN SoftSerial
+ESP8266 wifi(&Serial1);
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE); //Konfigurasi ADDRESS LCD i2c
 
 //Konfigurasi Auth Token BLYNK
-char auth[] = "6e42cc7dcc3b42349a1a53c895c30a08";
+char auth[] = "c01678a1ded946cdb3f12990c534d23f";
 
 //Konfigurasi Wifi
 char ssid[] = "hasanbasri93";
 char pass[] = "123456789";
 
+#define pin_arus 0;
+#define pin_vbatt 1;
+#define pin_vfan 2;
+#define pin_vsolar 3;
+
+
 void setup()
 {
-  Serial.begin(9600); //Memulai BAUDRATE SerialHard DEBUG
   setup_lcd();        //Memulai LCD i2c
-  EspSerial.begin(ESP8266_BAUD); //Memulai BAUDRATE SerialSoft ESP
-  Blynk.begin(auth, wifi, ssid, pass); //Memulai menghubing ke server BLYNK
   timer.setInterval(1000, info_lcd); //Konfigurasi, memanggail funsgi info_lcd per 1 detik
   timer.setInterval(1000, kirim_data); //Konfigurasi, memanggail funsgi kirim_data per 1 detik
+  Serial.begin(9600); //Memulai BAUDRATE SerialHard DEBUG
+  Serial1.begin(ESP8266_BAUD); //Memulai BAUDRATE SerialSoft ESP
+  Blynk.begin(auth, wifi, ssid, pass); //Memulai menghubing ke server BLYNK
 }
 
 void loop()
@@ -42,10 +52,10 @@ void loop()
 //Fungsi kirim_data, mengirim data ke server BLYNK
 void kirim_data()
 {
-  Blynk.virtualWrite(V0, cari_tegangan(0)); //Kirim data Tegangan batteri di PIN ANALOG yg disesuaikan
-  Blynk.virtualWrite(V1, cari_tegangan(1)); //Kirim data Tegangan batteri di PIN ANALOG yg disesuaikan
-  Blynk.virtualWrite(V2, cari_tegangan(2)); //Kirim data Tegangan batteri di PIN ANALOG yg disesuaikan
-  Blynk.virtualWrite(V3, cari_arus(3));     //Kirim data Arus di PIN ANALOG yg disesuaikan
+  Blynk.virtualWrite(V0, cari_tegangan(pin_vbatt)); //Kirim data Tegangan batteri di PIN ANALOG yg disesuaikan
+  Blynk.virtualWrite(V1, cari_tegangan(pin_vfan)); //Kirim data Tegangan batteri di PIN ANALOG yg disesuaikan
+  Blynk.virtualWrite(V2, cari_tegangan(pin_solar)); //Kirim data Tegangan batteri di PIN ANALOG yg disesuaikan
+  Blynk.virtualWrite(V3, cari_arus(pin_arus));     //Kirim data Arus di PIN ANALOG yg disesuaikan
 }
 
 //Fungsi setup_lcd, konfigurasi LCD i2c
@@ -56,9 +66,11 @@ void setup_lcd()
 
   //Papan Judul
   lcd.setCursor(0, 0);          //Atur kursor lcd pada baris ke 1 dan kolom ke 1
-  lcd.print("Hello, ARDUINO "); //Menampilkan info, silahkan diubah
+  lcd.print("ADE NOVIANTO"); //Menampilkan info, silahkan diubah
   lcd.setCursor(0, 1);          //Atur kursor lcd pada baris ke 2 dan kolom ke 1
-  lcd.print("Hello, ARDUINO "); //Menampilkan info, silahkan diubah
+  lcd.print("PROTOTYPE BCR"); //Menampilkan info, silahkan diubah
+  lcd.setCursor(0, 3);          //Atur kursor lcd pada baris ke 2 dan kolom ke 1
+  lcd.print("Connecting..."); //Menampilkan info, silahkan diubah
 }
 
 //Fungsi info_lcd, menampilakn pembacaan sensor2
@@ -66,22 +78,24 @@ void info_lcd()
 {
   lcd.setCursor(0, 0);      //Atur kursor lcd pada baris ke 1 dan kolom ke 1
   lcd.print("V_BATT   : "); //Menampilkan info, silahkan diubah
-  lcd.print(cari_tegangan(2));
-  lcd.setCursor(15, 0);                                 //Atur kursor lcd pada baris ke 1 dan kolom ke 16
-  int level_batt = map(level_batt, 12.7, 13.7, 0, 100); //Mapping pembacaan tegangan ke persentase; variable tegangan,tegangan terendah, tegangan tertinggi, persentase terendah,  persentase tertinggi
-  lcd.print(level_batt);                                //Menampilkan persentase battery level
+  lcd.print(cari_tegangan(pin_vbatt));
+  lcd.print(" V");
 
   lcd.setCursor(0, 1);      //Atur kursor lcd pada baris ke 2 dan kolom ke 1
   lcd.print("V_SURYA  : "); //Menampilkan info, silahkan diubah
-  lcd.print(cari_tegangan(1));
+  lcd.print(cari_tegangan(pin_vsolar));
+  lcd.print(" V");
 
   lcd.setCursor(0, 2);      //Atur kursor lcd pada baris ke 3 dan kolom ke 1
   lcd.print("V_KINCIR : "); //Menampilkan info, silahkan diubah
-  lcd.print(cari_tegangan(0));
+  lcd.print(cari_tegangan(pin_vfan));
+  lcd.print(" V");
 
   lcd.setCursor(0, 3);      //Atur kursor lcd pada baris ke 4 dan kolom ke 1
   lcd.print("ARUS     : "); //Menampilkan info, silahkan diubah
-  lcd.print(cari_arus(3));
+  lcd.print(cari_arus(pin_arus));
+  lcd.print(" A");
+
 }
 
 //Fungsi cari_tegangan, menghitung pembacaan tegangan dari PIN ANALOG
@@ -90,26 +104,22 @@ void info_lcd()
 float cari_tegangan(int pin)
 {
 
+
   //Konfigurasi variable - variable
   float vout = 0.0;
   float vin = 0.0;
-  float R1 = 30000.0;  //Nilai resistor 1 30K
-  float R2 = 7500.0;   //Nilai resistor 2 7.5K
-#define NUM_SAMPLES 10 //Variable jumlah sample data yg akan diambil, kali
-  int sum = 0;
-  unsigned char sample_count = 0;
-  float voltage = 0.0;
+  float R1 = 30000.0; //
+  float R2 = 7500.0; //
+  int value = 0;
 
-  while (sample_count < NUM_SAMPLES)
-  {
-    sum += analogRead(pin);
-    sample_count++;
-  }
+  vout = (value * 5.0) / 1024.0; // see text
+  vin = vout / (R2 / (R1 + R2));
 
-  voltage = (((float)sum / (float)NUM_SAMPLES * readVcc()) / 1024.0);
-  return voltage / (R2 / (R1 + R2));
-  sample_count = 0;
-  sum = 0;
+  value = analogRead(pin);
+  vout = (value * 5.0) / 1024.0; // see text
+  vin = vout / (R2 / (R1 + R2));
+`
+  return vin;
 }
 
 //Fungsi cari_arus, menghitung pembacaan ARUS dari PIN ANALOG
